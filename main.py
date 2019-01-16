7f326caff744da142ef6c5517b43a279d2c223b7
Jaspera1980 <33220584+Jaspera1980@users.noreply.github.com>
2019-01-16 16:44:37 +0100

    Created main file run all

--- main.py
+++ main.py
@@ -1,8 +1,52 @@
 from Furby import furby
+import time
+from time import sleep
+import Heart_beat_getter as bpm
+import numpy as np
+import os
+from random import choice
 import furby_vocabulary
-import Heart_beat_getter
-import Furby
+import scenario1
 
-furby_vocabulary.run_furby()
+#Heart rate threshold, the actual threshold will be determined in the onboarding
+bpm_threshold = 60
+
+#Scenarios where randomly can be choosen from
+intervention = [scenario1.scenario1]
+
+#Function to determine is scenario should be run or not
+def run_intervention():
+    #set initial heart rate to zero
+    heart_rate = bpm.heart_rate()
+    if heart_rate > bpm_threshold:
+        choice(intervention)()
+    else:
+        print('hartslag is %s, een intervention is niet nodig' %heart_rate)
+
+#Set timeout duration
+min_duration = 10 #seconds
+max_duration = 20 #seconds
+
+def main():
+    timeout = np.random.randint(min_duration, max_duration)
+    timeout_start = time.time()
+    while 1:
+        #After a set time the heart rate will measured and a random intervention will be suggested
+        #if the user is stressed (has an increased heart rate)
+        if time.time() > timeout_start + timeout:
+            #Run function here
+            run_intervention()
+            #Reset timer
+            timeout = np.random.randint(min_duration, max_duration)
+            timeout_start = time.time()
+            continue
+        else:
+            time.sleep(1)
+            text = furby.speech_to_text()
+            furby_vocabulary.vocabulary(text)
+            continue
+        
+main()
+
 
 
--- scenario1.py
+++ scenario1.py
@@ -4,8 +4,6 @@ from time import sleep
 import Heart_beat_getter as bpm
 import numpy as np
 import os
-from random import choice
-import furby_vocabulary
 
 #Scenario 1 script
 scen1_dict = {0: "Hoi! Voel je je misschien een beetje opgejaagd?",
@@ -93,44 +91,3 @@ def scenario1():
                 furby.speak(scen1_dict[7])
     if answer is 'nee':
         furby.speak(scen1_dict[2])
-
-
-#Variables
-bpm_threshold = 60
-
-#Scenarios where randomly can be choosen from
-intervention = [scenario1]
-
-#Function to determine is scenario should be run or not
-def run_intervention():
-    #set initial heart rate to zero
-    heart_rate = bpm.heart_rate()
-    if heart_rate > bpm_threshold:
-        choice(intervention)()
-    else:
-        print('hartslag is %s, een intervention is niet nodig' %heart_rate)
-
-#Set timeout duration
-min_duration = 10 #seconds
-max_duration = 20 #seconds
-
-def main():
-    timeout = np.random.randint(min_duration, max_duration)
-    timeout_start = time.time()
-    while 1:
-        #After a set time the heart rate will measured and a random intervention will be suggested
-        #if the user is stressed (has an increased heart rate)
-        if time.time() > timeout_start + timeout:
-            #Run function here
-            run_intervention()
-            #Reset timer
-            timeout = np.random.randint(min_duration, max_duration)
-            timeout_start = time.time()
-            continue
-        else:
-            time.sleep(1)
-            text = furby.speech_to_text()
-            furby_vocabulary.vocabulary(text)
-            continue
-
-main()
